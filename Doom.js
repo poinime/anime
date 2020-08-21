2b781f5ada21876569a8127c61ce73eccdb57216
poinime <69905953+poinime@users.noreply.github.com>
2020-08-21 20:05:25 +0700

    Artplayer.js

--- Doom.js
+++ Doom.js
@@ -20,7 +20,7 @@ var art = new Artplayer({
     fullscreenWeb: true,
     subtitleOffset: true,
     miniProgressBar: true,
-    localVideo: true,
+    localVideo: false,
     localSubtitle: true,
     networkMonitor: false,
     mutex: true,
@@ -40,20 +40,6 @@ var art = new Artplayer({
             },
         },
     ],
-    layers: [
-        {
-            html: `<img style="width: 100px" src="https://artplayer.org/assets/sample/layer.png">`,
-            click: function () {
-                console.info('You clicked on the custom layer');
-            },
-            style: {
-                position: 'absolute',
-                top: '20px',
-                right: '20px',
-                opacity: '.9',
-            },
-        },
-    ],
     quality: [
         {
             default: true,
@@ -65,50 +51,13 @@ var art = new Artplayer({
             url: 'https://artplayer.org/assets/sample/video.mp4',
         },
     ],
-    thumbnails: {
-        url: 'https://artplayer.org/assets/sample/thumbnails.png',
-        number: 100,
-        width: 160,
-        height: 90,
-        column: 10,
-    },
     subtitle: {
         url: 'https://artplayer.org/assets/sample/subtitle.srt',
         style: {
             color: '#03A9F4',
         },
     },
-    highlight: [
-        {
-            time: 60,
-            text: 'One more chance',
-        },
-        {
-            time: 120,
-            text: '谁でもいいはずなのに',
-        },
-        {
-            time: 180,
-            text: '夏の想い出がまわる',
-        },
-        {
-            time: 240,
-            text: 'こんなとこにあるはずもないのに',
-        },
-        {
-            time: 300,
-            text: '终わり',
-        },
-    ],
-    controls: [
-        {
-            position: 'right',
-            html: 'Control',
-            click: function () {
-                console.info('You clicked on the custom control');
-            },
-        },
-    ],
+    
     icons: {
         loading: '<img src="https://artplayer.org/assets/img/ploading.gif">',
         state: '<img src="https://artplayer.org/assets/img/state.png">',
